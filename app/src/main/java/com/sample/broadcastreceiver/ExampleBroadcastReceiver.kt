ed79d86ce6a26c4b138967d257ac887a167282f4
Brais Cidras <braisomary@gmail.com>
2020-08-28 11:59:10 +0200

    Adding CUSTOM BROADCASTS

--- app/src/main/java/com/sample/broadcastreceiver/ExampleBroadcastReceiver.kt
+++ app/src/main/java/com/sample/broadcastreceiver/ExampleBroadcastReceiver.kt
@@ -4,19 +4,13 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
-import android.util.Log
 import android.widget.Toast
 
 class ExampleBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        if (ConnectivityManager.CONNECTIVITY_ACTION == intent!!.action) {
-            var noConnectivity =
-                intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false)
-            if (noConnectivity) {
-                Toast.makeText(context, "Disconnected", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(context, "Connected", Toast.LENGTH_SHORT).show()
-            }
+        if ("com.sample.broadcastreceiver.EXAMPLE_ACTION" == intent!!.action) {
+            val receivedText = intent.getStringExtra("com.sample.broadcastreceiver.EXTRA_TEXT")
+            Toast.makeText(context, receivedText, Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/sample/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/sample/broadcastreceiver/MainActivity.kt
@@ -10,27 +10,20 @@ class MainActivity : AppCompatActivity() {
 
     private var exampleBroadcastReceiver = ExampleBroadcastReceiver()
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
     /**
      * With following lifecycle methods the broadcast receiver is alive while the activity is displayed
      * If we need the broadcast receiver alive even when the Activity is on the foreground: we should play with onCreate and onDestroy
      * If we want to keep it as much as our App is running then it should be registered on App.onCreate method
      */
-    override fun onStart() {
-        super.onStart()
-        // Registering the BroadcastReceiver
-        val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val filter = IntentFilter("com.sample.broadcastreceiver.EXAMPLE_ACTION")
         registerReceiver(exampleBroadcastReceiver, filter)
     }
 
-    override fun onStop() {
-        super.onStop()
-        // Unregistering the BroadcastReceiver
+    override fun onDestroy() {
+        super.onDestroy()
         unregisterReceiver(exampleBroadcastReceiver)
     }
 }
